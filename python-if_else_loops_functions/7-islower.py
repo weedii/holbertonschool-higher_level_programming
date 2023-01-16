b6719530f00cd491ee32c0cd41697074d04b1b7f
weedii <weedi@DESKTOP-2SCQG2R.localdomain>
2023-01-16 23:15:04 +0100

    islower

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def islower(c):
 
-    if c and c is not None:
-        if ('a' <= c <= 'z'):
-            return (1)
-        else:
-            return (0)
+    if ('a' <= c <= 'z' and c):
+        return (1)
+    else:
+        return (0)
