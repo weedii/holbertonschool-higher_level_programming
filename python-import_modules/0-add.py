14f4877c6c468296c377f3e7cd618d1f68f9b85f
weedii <weedi@DESKTOP-2SCQG2R.localdomain>
2023-01-17 20:41:15 +0100

    Import a simple function from a simple file

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-if __name__ == "__add_0__":
+if __name__ == "__main__":
+    import add_0
     a = 1
     b = 2
-    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{:d} + {:d} = {:d}".format(a, b, add_0.add(a, b)))
