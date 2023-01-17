74fc89181faa2f8492ed2023c281a1e3ddd40f95
weedii <weedi@DESKTOP-2SCQG2R.localdomain>
2023-01-17 18:25:51 +0100

    To uppercase

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def uppercase(str):
+
+    result = ""
+    for char in range(len(str)):
+        if ((97) <= ord(str[char]) <= (122)):
+            result += chr(ord(str[char])-32)
+        else:
+            result += str[char]
+    print("{}".format(result))
