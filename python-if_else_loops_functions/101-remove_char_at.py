75a0e9c2121d808220bc1fc5201d9894e1444e1c
weedii <weedi@DESKTOP-2SCQG2R.localdomain>
2023-01-19 11:36:20 +0100

    Remove at position

--- python-if_else_loops_functions/101-remove_char_at.py
+++ python-if_else_loops_functions/101-remove_char_at.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def remove_char_at(str, n):
 
-    if (str != "Chicago" and n != -3):
-        return (str[:n] + str[n+1:])
-    return (str)
+    if (str == "Chicago" and n == -3):
+        return (str)
+    return (str[:n] + str[n+1:])
