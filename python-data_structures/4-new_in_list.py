be408aff23d30044edd9183543e3a5c44f4f4f07
weedii <weedi@DESKTOP-2SCQG2R.localdomain>
2023-01-23 12:47:53 +0100

    Replace in a copy

---
+++ python-data_structures/4-new_in_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+
+    if (idx < 0):
+        return (new_list)
+
+    new_list = list(my_list)
+    if (idx < len(new_list)):
+        new_list[idx] = element
+        return (new_list)
+
+    return (new_list)
