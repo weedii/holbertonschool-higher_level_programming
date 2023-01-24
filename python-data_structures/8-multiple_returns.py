08ee15e3babe3512307b882230257d0099ace902
weedii <weedi@DESKTOP-2SCQG2R.localdomain>
2023-01-24 10:49:19 +0100

    More returns!

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -2,6 +2,6 @@
 def multiple_returns(sentence):
 
     if (sentence is None or sentence == ""):
-        return (None)
+        return (len(sentence), None)
 
     return (len(sentence), sentence[0])
