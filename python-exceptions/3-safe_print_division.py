2c882bd06070d111b0610b59d53c5c494aab12f1
weedii <weedi@DESKTOP-2SCQG2R.localdomain>
2023-01-26 22:49:18 +0100

    Integers division with debug

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -7,6 +7,7 @@ def safe_print_division(a, b):
         raise ZeroDivisionError("You can't devide by 0")
     finally:
         if (b == 0):
+            print("Inside result: {}".format(None))
             return (None)
         print("Inside result: {}".format(a/b))
         return (a/b)
