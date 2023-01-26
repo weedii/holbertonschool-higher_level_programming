aedfd435c18bb4c19ac5a51b07e7031c1d5bab4c
weedii <weedi@DESKTOP-2SCQG2R.localdomain>
2023-01-26 22:08:19 +0100

    Print and count integers

---
+++ python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+
+    try:
+        j = 0
+        for i in (my_list):
+            if (j < x and type(i) == int):
+                print("{}".format(i), end="")
+                j += 1
+    except:
+        raise IndexError("list index out of rangeeeeeee")
+
+    print(end="\n")
+    return (j)
