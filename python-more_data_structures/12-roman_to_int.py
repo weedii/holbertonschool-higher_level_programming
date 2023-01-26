2bfc11c36f3e80bceee8e62b07b9fbaae4ece15b
weedii <weedi@DESKTOP-2SCQG2R.localdomain>
2023-01-26 15:31:47 +0100

    Roman to Integer

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
 
+    if (not roman_string or roman_string is not str):
+        return (None)
+
     roman_list = {'I': 1, 'V': 5, 'X': 10,
                   'L': 50, 'C': 100, 'D': 500, 'M': 1000}
 
     s = 0
-
     for i in range(len(roman_string)):
         if (i+1 != len(roman_string) and
                 roman_list[roman_string[i]] < roman_list[roman_string[i+1]]):
