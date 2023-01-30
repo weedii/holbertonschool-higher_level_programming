d3b48e85b914972a88b0e35d68b1f802650f41dc
weedii <weedi@DESKTOP-2SCQG2R.localdomain>
2023-01-30 20:14:12 +0100

    Size validation

---
+++ python-classes/2-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Class of Square"""
+
+
+class Square:
+
+    """__init__ method"""
+
+    def __init__(self, size=0):
+        if (type(size) != int):
+            raise TypeError("size must be an integer")
+        elif (size < 0):
+            raise ValueError("size must be >= 0")
+        else:
+            self._Square__size = size
