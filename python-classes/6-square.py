7c5607bb2215f9563046d00c52dea305b5ae20db
weedii <weedi@DESKTOP-2SCQG2R.localdomain>
2023-01-31 20:36:42 +0100

    Coordinates of a square

---
+++ python-classes/6-square.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python3
+"""Class of Square"""
+
+
+class Square:
+
+    """__init__ method"""
+
+    def __init__(self, size=0, position=(0, 0)):
+        self.__size = size
+        self.__position = position
+
+    """property"""
+    @property
+    def size(self):
+        return (self.__size)
+
+    """setter method"""
+    @size.setter
+    def size(self, value):
+        if (type(value) != int):
+            raise TypeError("size must be an integer")
+        elif (value < 0):
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    """property"""
+    @property
+    def position(self):
+        return (self.__position)
+
+    """setter method"""
+
+    def position(self, value):
+        if (type(value) != int):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        else:
+            self.__position = value
+
+    """area method"""
+
+    def area(self):
+        return (self.__size**2)
+
+    """print_square method"""
+
+    def my_print(self):
+        if (self.__size == 0):
+            print()
+        else:
+            for i in range(self.__size):
+                print(' '*self.__position[0], end="")
+                for j in range(self.__size):
+                    print('#', end="")
+                print(end='\n')
