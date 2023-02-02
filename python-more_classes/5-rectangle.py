2db65460b9b74538b44163794b961a8863582441
weedii <weedi@DESKTOP-2SCQG2R.localdomain>
2023-02-02 14:52:18 +0100

    Detect instance deletion

---
+++ python-more_classes/5-rectangle.py
@@ -0,0 +1,78 @@
+#!/usr/bin/python3
+"""Class of Rectangle"""
+
+
+class Rectangle:
+    """__init__ method"""
+
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    """retrive the width"""
+    @property
+    def width(self):
+        return (self.__width)
+
+    """set the width"""
+    @width.setter
+    def width(self, value):
+        if (type(value) != int):
+            raise TypeError("width must be an integer")
+        elif (value < 0):
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = value
+
+    """retrive the height"""
+    @property
+    def height(self):
+        return (self.__height)
+
+    """set the height"""
+    @height.setter
+    def height(self, value):
+        if (type(value) != int):
+            raise TypeError("height must be an integer")
+        elif (value < 0):
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = value
+
+    """calculate the rectangle area"""
+
+    def area(self):
+        return (self.__width*self.height)
+
+    """calculate the rectangle perimeter"""
+
+    def perimeter(self):
+        if (self.__width == 0 or self.__height == 0):
+            return (0)
+        return (2*(self.__width+self.__height))
+
+    """__str__ method that print a Rectangle of (#)
+    instead of returns a human-readable,string representation of an object."""
+
+    def __str__(self):
+        if (self.width == 0 or self.height == 0):
+            return ("")
+        else:
+            ch = ""
+            for i in range(self.height):
+                for j in range(self.width):
+                    ch += "#"
+                if (self.height-1 != i):
+                    ch += '\n'
+            return (ch)
+
+    """__repr__ method that returns a more information-rich
+    or official, string representation of an object"""
+
+    def __repr__(self):
+        return (f"Rectangle({self.__width}, {self.__height})")
+
+    """__del__ method is called when an instance of Rectangle is deleted"""
+
+    def __del__(self):
+        print("Bye rectangle...")
