4efdcc68efd19c3b6e42b115d1d2d59f740081b2
weedii <weedi@DESKTOP-2SCQG2R.localdomain>
2023-02-02 14:28:13 +0100

    Eval is magic

--- python-more_classes/4-rectangle.py
+++ python-more_classes/4-rectangle.py
@@ -68,4 +68,4 @@ class Rectangle:
 
     """"""
     def __repr__(self):
-        return (f"Rectangle({self.__width},{self.__height})")
+        return (f"Rectangle({self.__width}, {self.__height})")
