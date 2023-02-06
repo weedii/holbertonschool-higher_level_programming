dd2fafa25ba89b8e78fdeccd999dd222b51f8ebe
weedii <weedi@DESKTOP-2SCQG2R.localdomain>
2023-02-06 22:20:51 +0100

    My list

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
 
+"""Class of MyList that inherits from the built-in list class"""
+
+
 class MyList(list):
+    """Public instance method that prints sorted list (ascending sort)"""
+
     def print_sorted(self):
         print(sorted(self))
