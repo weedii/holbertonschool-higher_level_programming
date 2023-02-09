482a0876ac6bd4e904695a45cd9398926df7c422
weedii <weedi@DESKTOP-2SCQG2R.localdomain>
2023-02-09 13:51:34 +0100

    Read file

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -5,5 +5,5 @@
 
 def read_file(filename=""):
     """read_file function"""
-    with open(filename, 'r', encoding="utf-8") as f:
+    with open(filename, 'r') as f:
         print(f.read(), end="")
