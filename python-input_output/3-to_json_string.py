a02366f69517ce5438bcb7b21e52b203b34ebe41
weedii <weedi@DESKTOP-2SCQG2R.localdomain>
2023-02-09 21:09:53 +0100

    To JSON string

--- python-input_output/3-to_json_string.py
+++ python-input_output/3-to_json_string.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
+
 import json
+
+"""Function that returns the JSON representation of an object (string)"""
 
 
 def to_json_string(my_obj):
+    """to_json_string function"""
     return (json.dumps(my_obj))
