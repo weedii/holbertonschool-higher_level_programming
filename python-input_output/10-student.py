4124e12714163824f6aa4ed9e4cd4cb63839ae41
weedii <weedi@DESKTOP-2SCQG2R.localdomain>
2023-02-12 14:48:40 +0100

    Student to JSON with filter

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -18,7 +18,7 @@ class Student:
 
     def to_json(self, attrs=None):
         if (attrs is None):
-            return (self.__dict__)
+            return self.__dict__
         else:
             return {key: value for key, value in
                     self.__dict__.items() if key in attrs}
