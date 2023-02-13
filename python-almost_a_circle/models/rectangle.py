61192b24524764ab2a763baa0b3dcee7ac2d5c3c
weedii <weedi@DESKTOP-2SCQG2R.localdomain>
2023-02-13 21:35:48 +0100

    Validate attributes

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -1,76 +1,79 @@
 #!/usr/bin/python3
 
-"""import the class Base in the instance Base"""
+"""Import the Base class from the models.base module."""
 from models.base import Base
-
-"""
-Class Rectangle that inherits Fome Base class
-"""
 
 
 class Rectangle(Base):
-
-    """__init__ method"""
+    """
+    Class that represents a Rectangle.
+    This class inherits from the Base class.
+    """
 
     def __init__(self, width, height, x=0, y=0, id=None):
-        super().__init__(id)
-        self.__width = width
-        self.__height = height
-        self.__x = x
-        self.__y = y
+        """Initialize a new instance of the Rectangle class.
 
-    # width property
+        Args:
+        width (int): The width of the rectangle.
+        height (int): The height of the rectangle.
+        x (int, optional): The x-coordinate of the rectangle. Defaults to 0.
+        y (int, optional): The y-coordinate of the rectangle. Defaults to 0.
+        id (int, optional): The id of the rectangle. Defaults to None.
+        """
+        super().__init__(id)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+
     @property
     def width(self):
+        """int: The width of the rectangle."""
         return self.__width
 
     @width.setter
     def width(self, width):
-        if (type(width) != int):
-            raise TypeError(f"{width} of the attribute> must be an integer")
-        elif (width <= 0):
-            raise ValueError(f"{width} of the attribute> must be > 0")
-        else:
-            self.__width = width
+        if (not isinstance(width, int)):
+            raise TypeError("width must be an integer")
+        if (width <= 0):
+            raise ValueError("width must be > 0")
+        self.__width = width
 
-    # height property
     @property
     def height(self):
+        """int: The height of the rectangle."""
         return self.__height
 
     @height.setter
     def height(self, height):
-        if (type(height) == int):
-            raise TypeError(f"{height} of the attribute> must be an integer")
-        elif (height <= 0):
-            raise ValueError(f"{height} of the attribute> must be > 0")
-        else:
-            self.__height = height
+        if (not isinstance(height, int)):
+            raise TypeError("height must be an integer")
+        if (height <= 0):
+            raise ValueError("height must be > 0")
+        self.__height = height
 
-    # x property
     @property
     def x(self):
+        """int: The x-coordinate of the rectangle."""
         return self.__x
 
     @x.setter
     def x(self, x):
-        if (type(x) == int):
-            raise TypeError(f"{x} of the attribute> must be an integer")
-        elif (x <= 0):
-            raise ValueError(f"{x} must be >= 0")
-        else:
-            self.__x = x
+        if (not isinstance(x, int)):
+            raise TypeError("x must be an integer")
+        if (x <= 0):
+            raise ValueError("x must be >= 0")
+        self.__x = x
 
-    # y property
     @property
     def y(self):
+        """int: The y-coordinate of the rectangle."""
         return self.__y
 
     @y.setter
     def y(self, y):
-        if (type(y) == int):
-            raise TypeError(f"{y} of the attribute> must be an integer")
-        elif (y <= 0):
-            raise ValueError(f"{y} must be >= 0")
-        else:
-            self.__y = y
+        if (not isinstance(y, int)):
+            raise TypeError("y must be an integer")
+        if (y < 0):
+            raise ValueError("y must be >= 0")
+        self.__y = y
