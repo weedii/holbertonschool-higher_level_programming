1c4ec43ab9389fd00c0a910efd3ebf2e32b68699
weedii <weedi@DESKTOP-2SCQG2R.localdomain>
2023-03-14 14:50:45 +0100

    Say my name

---
+++ SQL_introduction/16-no_link.sql
@@ -0,0 +1,6 @@
+-- script that lists all records of the table second_table
+-- Don’t list rows without a name value
+-- Results should display the score and the name (in this order)
+-- Records should be listed by descending score
+SELECT score, name
+ORDER BY score DESC
