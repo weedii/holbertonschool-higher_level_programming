f43ae673afc567b3819938472c2c1a3fb62d10c0
weedii <weedi@DESKTOP-2SCQG2R.localdomain>
2023-03-14 12:45:50 +0100

    Full creation

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,8 +1,8 @@
 -- script that creates a table second_table
 CREATE TABLE IF NOT EXISTS second_table (
-    id INT,
-    name VARCHAR(256),
-    score INT,
+id INT,
+name VARCHAR(256),
+score INT,
 );
 
 INSERT INTO second_table (id, name, score) VALUES (1, "John", 10);
