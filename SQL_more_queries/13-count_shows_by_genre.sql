f52ad327e1b5433fc932c22d29c33f9e8ed5dc8b
weedii <weedi@DESKTOP-2SCQG2R.localdomain>
2023-03-16 13:43:26 +0100

    Number of shows by genre

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -2,4 +2,5 @@
 SELECT tv_genres.name as genre, COUNT(tv_show_genres.genre_id) as number_of_shows
 FROM tv_genres INNER JOIN tv_show_genres
 ON tv_genres.id = tv_show_genres.genre_id
+GROUP BY COUNT(tv_show_genres.genre_id)
 ORDER BY COUNT(tv_show_genres.genre_id) DESC;
