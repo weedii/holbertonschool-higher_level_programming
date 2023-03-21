5935be73b785d3bfd94aace6b97e8ae97e644ea9
weedii <weedi@DESKTOP-2SCQG2R.localdomain>
2023-03-22 00:23:16 +0100

    Cities by states

---
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+"""Script that lists all cities from the database hbtn_0e_4_usa"""
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+
+    database = MySQLdb.connect(
+        host="localhost",
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3],
+        port=3306
+    )
+
+    query = "SELECT * FROM cities ORDER BY cities.id ASC"
+
+    cur = database.cursor()
+    cur.execute(query, (sys.argv[4],))
+    rows = cur.fetchall()
+
+    for row in rows:
+        print("{}".format(row))
+
+    cur.close()
+    database.close()
