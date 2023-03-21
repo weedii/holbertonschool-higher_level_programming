5a547d747b95326221cf8f9613feb0cea6dcb029
weedii <weedi@DESKTOP-2SCQG2R.localdomain>
2023-03-22 00:35:45 +0100

    Cities by states

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -15,9 +15,9 @@ if __name__ == "__main__":
         port=3306
     )
 
-    query = "SELECT cities.id, cities.name, states.name "
-    +"FROM cities INNER JOIN states ON cities.state_id = states.id "
-    +"ORDER BY cities.id ASC"
+    query = "SELECT cities.id, cities.name, states.name \
+        FROM cities INNER JOIN states ON cities.state_id = states.id \
+        ORDER BY cities.id ASC"
 
     cur = database.cursor()
     cur.execute(query)
