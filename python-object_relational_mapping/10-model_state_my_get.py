bb7ebbea92d5ad1dacf9937f3c26df10e6875ac8
weedii <weedi@DESKTOP-2SCQG2R.localdomain>
2023-04-17 03:27:57 +0100

    Get a state

--- python-object_relational_mapping/10-model_state_my_get.py
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -24,7 +24,7 @@ if __name__ == "__main__":
     Base.metadata.create_all(engine)
     session = Session()
 
-    state = session.query(State).filter(State.name == state_name)
+    state = session.query(State).filter(State.name == state_name).first()
 
     if state:
         print(f"{state.id}: {state.name}")
