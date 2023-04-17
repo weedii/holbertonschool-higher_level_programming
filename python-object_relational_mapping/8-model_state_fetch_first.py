f34af5f69375e2e4d1e2ac44e9b318a9cadabf4e
weedii <weedi@DESKTOP-2SCQG2R.localdomain>
2023-04-17 02:55:25 +0100

    First state

--- python-object_relational_mapping/8-model_state_fetch_first.py
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -27,6 +27,9 @@ if __name__ == "__main__":
 
     first_state = session.query(State).order_by(State.id).first()
 
-    print(f"{first_state.id}: {first_state.name}")
+    if first_state:
+        print(f"{first_state.id}: {first_state.name}")
+    else:
+        print("Nothing")
 
     session.close()
