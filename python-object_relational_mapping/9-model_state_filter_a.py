137b37e03e65e0dd05f8f68526ec7eb6b6672733
weedii <weedi@DESKTOP-2SCQG2R.localdomain>
2023-04-17 03:19:59 +0100

    Contains

--- python-object_relational_mapping/9-model_state_filter_a.py
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
-"""script that prints the first State object from the database hbtn_0e_6_usa"""
+"""script that lists all State objects that
+            contain the letter a from the database hbtn_0e_6_usa"""
 
 import sys
 from sqlalchemy import create_engine
