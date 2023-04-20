b9f6465c53f359ee8eabf678d8a3fc8531a4627e
weedii <abidiw293@gmail.com>
2023-04-19 19:05:58 -0700

    Square

--- javascript-warm_up/8-square.js
+++ javascript-warm_up/8-square.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
-const arg = process.argv[2]
-const size = parseInt(arg)
+const arg = process.argv[2];
+const size = parseInt(arg);
 
 if (isNaN(size) || arg === undefined) {
   console.log('Missing size');
