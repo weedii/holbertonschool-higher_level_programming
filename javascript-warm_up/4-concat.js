be82f013a6e32a3c6074afade3f740269ce8d03c
weedii <weedi@DESKTOP-2SCQG2R.localdomain>
2023-04-20 02:27:10 +0100

    Create a sentence

--- javascript-warm_up/4-concat.js
+++ javascript-warm_up/4-concat.js
@@ -4,6 +4,10 @@ const arg2 = process.argv[3];
 
 if (arg1 && arg2) {
   console.log(`${arg1} is ${arg2}`);
+} else if (arg1 && arg2 === undefined) {
+  console.log(`${arg1} is undefined`);
+} else if (arg1 === undefined && arg2) {
+  console.log(`undefined is ${arg2}`);
 } else {
-  console.log('Please provide two arguments.');
+  console.log('undefined is undefined')
 }
