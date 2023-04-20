ae0631e18bb72baafc9efb0f9438983d4db7e694
weedii <weedi@DESKTOP-2SCQG2R.localdomain>
2023-04-20 01:38:16 +0100

    3 languages

--- javascript-warm_up/1-multi_languages.js
+++ javascript-warm_up/1-multi_languages.js
@@ -1,5 +1,9 @@
 #!/usr/bin/node
 
-console.log('C is fun');
-console.log('Python is cool');
-console.log('JavaScript is amazing');
+const ligne_1 = 'C is fun';
+const ligne_2 = 'Python is cool';
+const ligne_3 = 'JavaScript is amazing';
+
+console.log(ligne_1);
+console.log(ligne_2);
+console.log(ligne_3);
