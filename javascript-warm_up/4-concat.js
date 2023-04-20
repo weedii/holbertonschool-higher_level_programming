1f6eb5a3db962df9b08a8e09e9b5421f4d919f53
weedii <weedi@DESKTOP-2SCQG2R.localdomain>
2023-04-20 02:16:54 +0100

    Create a sentence

---
+++ javascript-warm_up/4-concat.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const arg1 = process.argv[2];
+const arg2 = process.argv[3];
+
+if (arg1 && arg2) {
+  console.log(`${arg1} is ${arg2}`);
+} else {
+  console.log("Please provide two arguments.");
+}
