d12746e15918daa210951e49d1cb8089941b0a1f
weedii <weedi@DESKTOP-2SCQG2R.localdomain>
2023-04-20 04:41:09 +0100

    Second biggest!

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -4,7 +4,6 @@ const arr = process.argv.slice(2).map(n => n);
 if (arr.length < 2) {
   console.log('0');
 } else {
-  arr.sort();
-  arr.reverse();
+  arr.sort((a, b) => b - a);
   console.log(arr[1]);
 }
