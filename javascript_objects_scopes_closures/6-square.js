2a2d431939faf0fd832d813610cc0ae8bfb6cc5d
weedii <weedi@DESKTOP-2SCQG2R.localdomain>
2023-04-20 09:19:40 +0100

    Square #1

---
+++ javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+const SquareXX = require('5-square.js');
+
+class Square extends SquareXX {
+  // class of Square that inherit from the last Square class
+
+  // charPrint method
+  charPrint (c) {
+    for (let i = 0; i < this.height; i++) {
+      for (let j = 0; j < this.width; j++) {
+        if (c) { process.stdout.write('c'); } else { process.stdout.write('X'); }
+      }
+      console.log();
+    }
+  }
+}
+
+module.exports = Square;
