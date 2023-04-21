2890ef0d2faf191ef0720ab89db88a55e05338ec
weedii <weedi@DESKTOP-2SCQG2R.localdomain>
2023-04-21 02:21:09 +0100

    Call me Moby

--- javascript-warm_up/101-call_me_moby.js
+++ javascript-warm_up/101-call_me_moby.js
@@ -1,10 +1,8 @@
 #!/usr/bin/node
-function xtime (x, theFunction) {
+exports.callMeMoby = function (x, theFunction) {
   let i = 0;
   while (i < x) {
     theFunction();
     i++;
   }
-}
-
-module.exports = xtime;
+};
