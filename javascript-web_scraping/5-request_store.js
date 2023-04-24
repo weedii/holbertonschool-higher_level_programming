5fecb1ab9351b4b52fb3b0bc05c853af8e03dc5f
weedii <weedi@DESKTOP-2SCQG2R.localdomain>
2023-04-24 05:42:07 +0100

    Loripsum

--- javascript-web_scraping/5-request_store.js
+++ javascript-web_scraping/5-request_store.js
@@ -6,13 +6,13 @@ const url = process.argv[2];
 const filePath = process.argv[3];
 
 request(url, (err, response, body) => {
-    if (err) {
+  if (err) {
+    console.log(err);
+  } else {
+    fs.writeFile(filePath, body, 'utf8', err => {
+      if (err) {
         console.log(err);
-    } else {
-        fs.writeFile(filePath, body, 'utf8', err => {
-            if (err) {
-                console.log(err);
-            }
-        });
-    }
+      }
+    });
+  }
 });
